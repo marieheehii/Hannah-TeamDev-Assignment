264441c64d8087e81178364d92c867e2e1c7e80e
Hannah Young <hamyoun@iu.edu>
2022-03-17 20:43:06 -0400

    Add Repo Layer1

--- src/DevTeams.Repository/DevTeamRepo.cs
+++ src/DevTeams.Repository/DevTeamRepo.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DevTeams.Repository
-{
+
     public class DevTeamRepo
     {
-        
+        private readonly List<DevTeam> _devTeamsDatabase = new List<DevTeam>();
+
+        private int _count=0;
+        public bool AddDevTeamToDataBase(DevTeam devTeam)
+        {
+            if(devTeam !=null)
+            {
+                _count++;
+                _devTeamsDatabase.Add(devTeam);
+                return true;
+            }else{
+                return false;
+            }
+        }
+        public List<DevTeam> GetAllDevTeams()
+        {
+            return _devTeamsDatabase;
+        }
+        public DevTeam GetDevTeamByID(int id)
+        {
+            foreach(var devTeam in _devTeamsDatabase)
+            {
+                if(devTeam.ID == id)
+                {
+                    return devTeam;
+                }
+            }
+            return null;
+        }
+        public bool RemoveDevTeamFromDatabase(int id)
+        {
+            var devTeam = GetDevTeamByID(id);
+            if(devTeam != null)
+            {
+                _devTeamsDatabase.Remove(devTeam);
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
     }
-}
--- src/DevTeams.Repository/DeveloperRepo.cs
+++ src/DevTeams.Repository/DeveloperRepo.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DevTeams.Repository
-{
+
     public class DeveloperRepo
     {
-        
+        private readonly List<Developer> _developerDatabase = new List<Developer>();
+
+        private int _count=0;
+        public bool AddDeveloperToDataBase(Developer developer)
+        {
+            if(developer != null)
+            {
+                _count++;
+                developer.ID=_count;
+                _developerDatabase.Add(developer);
+                return true;
+            }else{
+                return false;
+            }
+        }
+        public List<Developer> GetAllDevelopers()
+        {
+            return _developerDatabase;
+        }
+        public Developer GetDeveloperByID(int id)
+        {
+            foreach(var developer in _developerDatabase)
+            {
+                if(developer.ID == id)
+                {
+                    return developer;
+                }return null;
+            }
+        }
+        public bool RemoveDeveloperFromDatabase(int id)
+        {
+            var developer = GetDeveloperByID(id);
+            if(developer != null)
+            {
+                _developerDatabase.Remove(developer);
+                return true;
+            }else{
+                return false;
+            }
+
+        }
     }
-}
